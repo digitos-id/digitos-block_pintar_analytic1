a6d95471806f8029d865cf790d265546db830e74
prihantosa <toosa@digitos.id>
2022-09-13 19:44:44 +0700

    Tambah overview1.php yang berisi chart draft

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2022082201;
+$plugin->version   = 2022082202;
 $plugin->requires  = 2020061500;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.9 onwards';
