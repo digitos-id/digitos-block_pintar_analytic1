77006aefef075cdd5b4376577d670c64e294a24f
prihantosa <toosa@digitos.id>
2022-09-14 06:42:36 +0700

    require_once(->dirroot & version 2022091401

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2022091307;
+$plugin->version   = 2022091401;
 $plugin->requires  = 2020061500;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.9 onwards';
